ba7648a7f6d44680290bf8cd0ee0afac3bc600ea
Andy Yang <2878847069@qq.com>
2025-12-02 13:57:29 +0800

    feat(components): add shadcn UI configuration and dependencies
    - Add `components.json` to configure shadcn UI with Tailwind CSS and React setup
    - Include necessary dependencies such as `@radix-ui`, `lucide-react`, `tailwind-merge`, and `clsx`
    - Add dev dependency `tw-animate-css` for animation utilities
    - Update package.json with new component-related libraries and configurations

---
+++ src/pages/UI.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@components/ui/button'
+export default function UI() {
+	return <div>
+		<Button variant="default">Click me</Button>
+		<Button variant="outline">Click me</Button>
+		<Button variant="secondary">Click me</Button>
+		<Button variant="ghost">Click me</Button>
+		<Button variant="link">Click me</Button>
+	</div>
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,13 +2,18 @@ import {
   createBrowserRouter,
 } from "react-router"
 import App from '@pages/App.tsx'
+import UI from '@pages/UI.tsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
 		// element: <App />,
 		Component: App,
-  }
+  },
+	{
+		path: "/ui",
+		Component: UI,
+	}
 ])
 
 export default router
